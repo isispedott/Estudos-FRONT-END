6d7b6b3e3e39198cb6f0a6cf0e442085e479fb74
Isis Pedott <101114312+isispedott@users.noreply.github.com>
2022-12-07 15:40:18 -0300

    studies

---
+++ JavaScript - Udemy/aula29/arrays.js
@@ -0,0 +1,12 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+
+alunos[0] = 'Eduardo';
+ console.log(alunos);
+
+
+
+
+
+//console.log(alunos);
+//console.log(alunos[0]);
+//console.log(alunos[2]);
---
+++ JavaScript - Udemy/functions.js
@@ -0,0 +1,27 @@
+/*function saudacao(nome) {
+    console.log(`Bom dia, ${nome}`)
+}
+
+saudacao('Luiz')
+saudacao('Maria') */
+
+/*function soma(x, y) {
+    const resultado = x + y;
+    return resultado
+}
+
+console.log(soma(2, 2))*/
+
+/*function soma(x, y) {
+    const resultado = x + y;
+    return resultado
+}
+
+console.log (soma(2, 2)); */
+
+const raiz = function(n) {
+    return n ** 0.5
+
+}
+
+console.log (raiz(9)); 
