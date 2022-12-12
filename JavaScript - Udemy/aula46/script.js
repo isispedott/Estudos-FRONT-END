2f93c87de0e4e8458371f2b563cc9d8b22df4fc7
Isis Pedott <101114312+isispedott@users.noreply.github.com>
2022-12-12 15:55:44 -0300

    update classes

--- JavaScript - Udemy/aula46/script.js
+++ JavaScript - Udemy/aula46/script.js
@@ -3,7 +3,7 @@
 //const data = new Date(0 + tresHoras - umDia);
 //const data = new Date(2019, 3); // ano, mês, dia, hora, Metro, segundo, metro/segundo
 
-const data = new Date('2019-04-20 20:15:59.100');
+/*const data = new Date('2019-04-20 20:15:59.100');
 console.log('Dia', data.getDate());
 console.log('Mês', data.getMonth() + 1); //mês começa do zero
 console.log('Ano', data.getFullYear());
@@ -12,7 +12,7 @@ console.log('Min', data.getMinutes());
 console.log('Seg', data.getSeconds());
 console.log('ms', data.getMilliseconds());
 console.log('Dia Semana', data.getDay()); // 0 - Domingo, 6 - Sábado
-console.log(data.toString());
+console.log(data.toString()); */
 
 
 
@@ -20,3 +20,22 @@ console.log(data.toString());
 
 //const data = new Date(0); // 01/01/1970 Timestamp unix/ época unix
 //console.log(data.toString());
+
+function zeroAEsquerda (num){
+    return num >= 10 ? num : `0${num}`;
+}
+
+function formataData(data) {
+    const dia = zeroAEsquerda(data.getDate());
+    const mes = zeroAEsquerda(data.getMonth() + 1);
+    const ano = zeroAEsquerda(data.getFullYear()); 
+    const hora = zeroAEsquerda(data.getHours());
+    const min = zeroAEsquerda(data.getMinutes());
+    const seg = zeroAEsquerda(data.getSeconds());
+
+    return `${dia}/ ${mes} / ${ano} ${hora}:${min}:${seg}`
+}
+
+const data = new Date();
+const dataBrasil = formataData(data);
+console.log(dataBrasil);
